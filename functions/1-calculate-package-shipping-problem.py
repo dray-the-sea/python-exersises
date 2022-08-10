c978e6dd84516e88ce51e9542dd57260fe344af4
dray-the-sea <109749397+dray-the-sea@users.noreply.github.com>
2022-08-10 11:50:55 -0500

    function to calculate package shipping costs

---
+++ functions/1-calculate-package-shipping-problem.py
@@ -0,0 +1,24 @@
+# write a function that calculates shiping cost given the weight of a package and the distance it's traveling
+# shipping cost rules:
+#  1. there's a starting cost of $5 per package, regardless of weight and distance
+#  3. add $2 for every 5lb if the package is over 1lb
+#  3. add $.25 for every 10 miles if the distance is over 12 miles
+#
+
+
+def ...
+
+
+
+
+# test the fuction with the following shipping options: 
+#  1. letter that weighs .3lb and is traveling 5 miles
+#  2. letter that weighs .3lb and is traveling 12 miles
+#  3. letter that weighs .25lb and is traveling 356 miles
+#  4. box that weighs 5lb and is traveling 12 miles
+#  5. box that weighs 5lb and is traveling 8 miles
+#  6. box that weighs 5lb and is traveling 800 miles
+#  7. box that weighs 90lb and is traveling 1 miles
+#  8. box that weighs 32lb and is traveling 942 miles
+#  9. box that weighs 55lb and is traveling 12 miles
+
