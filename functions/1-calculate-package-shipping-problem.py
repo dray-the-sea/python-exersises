af1774a45a56b0ed79f174e81f7434165d940bf6
dray-the-sea <109749397+dray-the-sea@users.noreply.github.com>
2022-08-11 11:17:30 -0500

    small update to the calc package problem and adding a problem that require use of functions

--- functions/1-calculate-package-shipping-problem.py
+++ functions/1-calculate-package-shipping-problem.py
@@ -1,24 +1,23 @@
-# write a function that calculates shiping cost given the weight of a package and the distance it's traveling
+# write a function that returns shiping cost given the weight of a package and the distance it's traveling
 # shipping cost rules:
 #  1. there's a starting cost of $5 per package, regardless of weight and distance
-#  3. add $2 for every 5lb if the package is over 1lb
-#  3. add $.25 for every 10 miles if the distance is over 12 miles
-#
+#  3. for packages is over 5lb, add $2 for every additional 1lb
+#  3. for distance is over 12 miles, add $.25 for every additional 1 mile 
 
 
-def ...
+
+
 
 
 
 
 # test the fuction with the following shipping options: 
-#  1. letter that weighs .3lb and is traveling 5 miles
-#  2. letter that weighs .3lb and is traveling 12 miles
-#  3. letter that weighs .25lb and is traveling 356 miles
-#  4. box that weighs 5lb and is traveling 12 miles
-#  5. box that weighs 5lb and is traveling 8 miles
-#  6. box that weighs 5lb and is traveling 800 miles
-#  7. box that weighs 90lb and is traveling 1 miles
-#  8. box that weighs 32lb and is traveling 942 miles
-#  9. box that weighs 55lb and is traveling 12 miles
-
+#  1. letter that weighs .3lb and is traveling 5 miles - expected cost: $5
+#  2. letter that weighs .3lb and is traveling 12 miles - expected cost: $5
+#  3. letter that weighs .3lb and is traveling 52 miles - expected cost: $15
+#  4. box that weighs 5lb and is traveling 8 miles - expected cost: $5
+#  5. box that weighs 5lb and is traveling 12 miles - expected cost: $5
+#  6. box that weighs 5lb and is traveling 800 miles - expected cost: $202
+#  7. box that weighs 25lb and is traveling 1 miles - expected cost: $45
+#  8. box that weighs 25lb and is traveling 12 miles - expected cost: $45
+#  9. box that weighs 25lb and is traveling 800 miles - expected cost: $242
--- functions/1-calculate-package-shipping-solution.py
+++ functions/1-calculate-package-shipping-solution.py
@@ -1,9 +1,8 @@
-# write a function that calculates shiping cost given the weight of a package and the distance it's traveling
+# write a function that returns shiping cost given the weight of a package and the distance it's traveling
 # shipping cost rules:
 #  1. there's a starting cost of $5 per package, regardless of weight and distance
-#  3. for packages is over 5lb, add $2 for every 1lb
-#  3. for distance is over 12 miles, add $.25 for every 1 mile 
-
+#  3. for packages is over 5lb, add $2 for every additional 1lb
+#  3. for distance is over 12 miles, add $.25 for every additional 1 mile 
 
 
 def calculate_shipping(weight, distance):
@@ -19,9 +18,11 @@ def calculate_shipping(weight, distance):
 
     if weight > free_weight:
         cost += lb_surcharge * (weight - free_weight)
+        print("weight surcharge: ", lb_surcharge * (weight - free_weight))
     
     if distance > free_distance:
         cost += distance_surcharge * (distance - free_distance)
+        print ("distance surcharge: ", distance_surcharge * (distance - free_distance))
 
     return cost
 
