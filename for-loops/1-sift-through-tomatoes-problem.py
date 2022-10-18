45da5d4062ac88aa3093bcd90fe38f8c1636251b
dray-the-sea <109749397+dray-the-sea@users.noreply.github.com>
2022-10-18 13:17:08 -0500

    de-linting some files to see the action run again.

--- for-loops/1-sift-through-tomatoes-problem.py
+++
@@ -1,7 +0,0 @@
-# Go through a pile of boxes of tomatoes and print what box it's in and whether it's rotten or not. 
-
-boxes_of_tomatoes = [
-                    ["rotten", "not rotten", "not rotten", "not rotten", "rotten"],
-                    ["not rotten", "rotten", "rotten", "rotten", "not rotten"],
-                    ["rotten", "rotten", "rotten", "not rotten", "not rotten", "not rotten"]
-                    ]
